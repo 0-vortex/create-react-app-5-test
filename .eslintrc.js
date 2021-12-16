1351325c5b4293de0c1155d2f67db2ec639b4078
TED Vortex <ted.vortex@gmail.com>
2021-12-17 01:44:24 +0200

    feat: eject deps

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  "extends": [
+      "react-app"
+  ],
+  "rules": {
+      "no-restricted-globals": [1]
+  },
+};
---
+++ src/lib/reducers.js
@@ -0,0 +1,31 @@
+import {useCallback} from "react";
+import {usePersistedState} from "./hooks";
+
+export function goalsReducer(state, action) {
+  if (state.errors) {
+    return {error: `"${state.errors[0].message}"`};
+  }
+  switch (action.type) {
+    case "GET":
+      return state.repository ? state : state.data.gitHub.viewer;
+    case "UPDATE":
+      return state;
+    case "CREATE":
+      return state.data.createRepository;
+    default:
+      throw new Error("No Action was provided.");
+  }
+}
+
+export function usePersistentStateReducer(bucket, reducer) {
+  const [state, setState] = usePersistedState(bucket);
+
+  const dispatch = useCallback(
+    action => {
+      return setState(reducer(state, action));
+    },
+    [reducer, state],
+  );
+
+  return [state, dispatch];
+}
