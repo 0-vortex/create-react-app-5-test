d2274625913d545108515ad1e871b214befc3d83
TED Vortex <ted.vortex@gmail.com>
2021-12-17 05:10:52 +0200

    perf: add correct service worker configuration

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Config from "./config";
 import {getUserFromJwt} from "./lib/identityActions";
 import './index.css';
 import "react-loading-skeleton/dist/skeleton.css";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import OneGraphApolloClient from "onegraph-apollo-client";
 import {ApolloProvider, InMemoryCache} from "@apollo/client";
@@ -25,7 +25,7 @@ function Index() {
 
     useEffect(() => {
         console.log(`%c
- ██████╗ ██████╗ ███████╗███╗   ██╗    ███████╗ █████╗ ██╗   ██╗ ██████╗███████╗██████╗ 
+ ██████╗ ██████╗ ███████╗███╗   ██╗    ███████╗ █████╗ ██╗   ██╗ ██████╗███████╗██████╗
 ██╔═══██╗██╔══██╗██╔════╝████╗  ██║    ██╔════╝██╔══██╗██║   ██║██╔════╝██╔════╝██╔══██╗
 ██║   ██║██████╔╝█████╗  ██╔██╗ ██║    ███████╗███████║██║   ██║██║     █████╗  ██║  ██║
 ██║   ██║██╔═══╝ ██╔══╝  ██║╚██╗██║    ╚════██║██╔══██║██║   ██║██║     ██╔══╝  ██║  ██║
@@ -120,8 +120,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-registerServiceWorker();
