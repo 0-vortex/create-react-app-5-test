60067ce6e4fc26f560a384e8d6186edeb22d3334
TED Vortex <ted.vortex@gmail.com>
2021-12-17 05:45:49 +0200

    chore: add coverage report

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,19 @@ module.exports = {
   ],
   "collectCoverageFrom": [
     "src/**/*.{js,jsx,ts,tsx}",
-    "!src/**/*.d.ts"
+    "!<rootDir>/node_modules/",
+    "!src/**/*.d.ts",
+    "!src/(icons|illustrations|images|logos|styles|tests)/**/*"
   ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 30,
+      "functions": 30,
+      "lines": 30,
+      "statements": 30
+    }
+  },
+  "coverageReporters": ["text"],
   "setupFiles": [
     "react-app-polyfill/jsdom"
   ],
